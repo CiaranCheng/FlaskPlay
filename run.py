f9cf34ac9479cf5d41d8812aab40f4987f0151d4
CiaranCheng <3169632278@qq.com>
2020-07-27 22:01:42 +0800

    feat:初次调试

--- run.py
+++ run.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'Hello World'
+   return 'Hello XBB'
+@app.route('/index')
+def hello_world():
+   return 'Hello XBB'
 
+# 注意这里，app.run默认的主机是127.0.0.1,是不能被外部访问的
 if __name__ == '__main__':
-   app.run()
+   app.run(host='0.0.0.0', port=5000)

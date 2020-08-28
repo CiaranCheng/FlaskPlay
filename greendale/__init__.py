3a824233250f29efd0c85cee554f1358c9ccbd41
CiaranCheng <1095074672@qq.com>
2020-08-28 17:28:33 +0800

    feat:单据查询tml

--- greendale/__init__.py
+++ greendale/__init__.py
@@ -1,7 +1,23 @@
 from flask import Flask
 app = Flask(__name__)
 from greendale import routes
-
 # 需要注意的有以下几点
 # routes需要在最下方引用和导入 这是为了解决 循环导入的问题
-# 
+
+# import pymssql
+# from flask_sqlalchemy import SQLAlchemy
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pymssql://lc0049999:cwpass12!@localhost:1433/cwbase4?charset=utf8'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# db = SQLAlchemy(app)
+
+# conn = pymssql.connect(
+#     host='127.0.0.1',
+#     user='lc0049999',
+#     password='cwpass12!',
+#     db='cwbase4',
+#     charset='utf8'
+# )
+# def get_conn():
+#     conn = pymssql.connect("localhost", "root", "root@123", "jike")
+#     return conn
--- greendale/routes.py
+++ greendale/routes.py
@@ -1,5 +1,17 @@
 from greendale import app
 from flask import render_template
+
+
+import pymssql
+
+conn = pymssql.connect(
+    host='127.0.0.1',
+    user='lc0519999',
+    password='cwpass12!',
+    database='cwbase51',
+    charset='utf8'
+)
+# conn = pymssql.connect(host='127.0.0.1:1433',user='sa',password='123456',database='NEWPFJDB',charset='UTF-8')
 @app.route('/')
 
 @app.route('/index')
@@ -13,7 +25,19 @@ def index():
 @app.route('/simplate')
 def simplate():
     return render_template('simplate.html',saying = 'alllhell:')
-
-
-
 # 实际上这里使用的render_template是flask所用的原生的Jinja2模版引擎
+
+@app.route('/data')
+def getdata():
+    cur = conn.cursor()
+    tablename = "CGDD1"
+    sql = "select * from " + tablename
+    cur.execute(sql)
+    content = cur.fetchall()
+	# 获取表头
+    sql = "Select Name FROM SysColumns Where id=Object_Id('"+tablename+"')"
+    cur.execute(sql)
+    labels = cur.fetchall()
+    labels = [l[0] for l in labels]
+    return render_template('tabletest.html', tablename = tablename ,labels=labels, content=content)
+
